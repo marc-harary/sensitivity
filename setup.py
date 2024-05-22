9cd61eda93e3ab151a2d339d87cd8fcd28861fa4
Marc Harary <marcharary@Marcs-MacBook-Pro.local>
2024-05-21 21:01:14 -0400

    Cleaned up repo

--- setup.py
+++ setup.py
@@ -5,12 +5,15 @@ ext_modules = [
     Extension(
         name="primary_sensitivity",
         sources=["sensitivity/sensitivity.pyx", "sensitivity/students_t.c", "sensitivity/primary_sensitivity_impl.c"],
-        include_dirs=["."],  # This ensures the headers are found in the current directory
+        include_dirs=["."],
     )
 ]
 
 setup(
     name="primary_sensitivity",
-    ext_modules=cythonize(ext_modules),
+    version="0.1",
+    description="Computes sensitivity of Pearson Correlation Coefficient and its p-value",
+    author="Marc Harary",
+    author_email="marc@ds.dfci.harvard.edu",
+    ext_modules=cythonize(ext_modules)
 )
-
